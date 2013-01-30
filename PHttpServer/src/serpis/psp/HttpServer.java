40747eb5110de03ea06afb5995366427b464c98e
Daniel Mayans <dani.mb90@gmail.com>
2013-01-30 17:49:16 +0100

    actualizacion

--- PHttpServer/src/serpis/psp/HttpServer.java
+++ PHttpServer/src/serpis/psp/HttpServer.java
@@ -1,70 +1,90 @@
 package serpis.psp;
-
+import java.io.IOException;
+import java.util.Scanner;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-
-//VERSIÓN MONOHILO
 
 public class HttpServer 
 {
+	private static final String newLine = "\r\n";
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		final String newLine = "\r\n";
+		//final String newLine = "\r\n";
 		final int port = 8080;
-		final String fileNameError404 = "fileError404.html";
-		final String response200 = "HTTP/1.0 200 OK";
-		final String response404 = "HTTP/1.0 404 Not Found";
-		
 		ServerSocket serverSocket = new ServerSocket (port);
+		String threadName = Thread.currentThread().getName();
+		System.out.println("threadName" + threadName);
 		
 		while (true) {
-			//Que el servidor se quede escuchando
 			Socket socket = serverSocket.accept();
+			String fileName = getFileName(socket.getInputStream());
+			writeHeader(socket.getOutputStream(), fileName);
+			writeFile(socket.getOutputStream(), fileName);
+			socket.close();
+		}
+		//serverSocket.close();
+	}
+		private static String getFileName(InputStream inputStream){
+			Scanner scanner = new Scanner (inputStream);
+			//String fileName = "index.html";
+			String fileName = " ";
+			final String defaultFileName = "index.html";
 			
-			
-			Scanner scanner = new Scanner (socket.getInputStream() );
-			
-			String fileName = "index.html";
-			
+			//GET
 			while(true){
 				String line = scanner.nextLine();
-				System.out.println(line);
-				if (line.equals(""))
+				//System.out.println(line);
+				if (line.contains("GET")){ //GET /index.html HTTP 1/1
+					fileName=line.split(" ")[1].substring(1); //Se obtiene index.html
+					/*
+					String
+					int index = 5;
+					while(line.charAt[index] != ' ')
+					fileName += line.charAt(index++);
+					Pattern pattern = Pattern.compile("GET /(?<filename>.*) HTTP/1.[01"];
+					Matcher marcher = pattern.matcher(line);
+					filename = matcher.group(1); //from 1.7 -> marcher. group("filename");
+					fileName = line.substring(5, line.indexOf(" ", 5));
+					*/
+				}
+				if (line.equals("")){
 					break;
+				}
 			}
+			//return fileName.equals("") ? fileName : defaultFileName;	
+			if (fileName.equals(""))
+				fileName = defaultFileName;
+			System.out.println("fileName=" + fileName);
+			return fileName;
+		}
 		
-			File file = new File(fileName);
-			
-			String responseFileName = file.exists() ? fileName : fileNameError404;
+		private static void writeHeader(OutputStream outputStream, String fileName) throws IOException{
+			final String response200 = "HTTP/1.0 200 OK";
+			final String response404 = "HTTP/1.0 404 Not Found";
+			File file = new File (fileName);
 			String response = file.exists() ? response200 : response404;
-			
 			String header = response + newLine + newLine;
 			byte[] headerBuffer = header.getBytes();
-			
-			OutputStream outputStream = socket.getOutputStream();
 			outputStream.write(headerBuffer);
-			
+		}
+		
+		private static void writeFile(OutputStream outputStream, String fileName) throws IOException, InterruptedException{
+			final String fileNameError404 = "fileError404.html";
+			File file = new File(fileName);
+			String responseFileName = file.exists() ? fileName : fileNameError404;
 			final int bufferSize = 2048;
 			byte[] buffer = new byte[bufferSize];
-			
 			FileInputStream fileInputStream = new FileInputStream(responseFileName);
-			
 			int count;
-			
 			while ((count = fileInputStream.read(buffer)) != -1){
-				//Retardo
+				//Retraso en milisegundos
 				Thread.sleep (3000); 
 				outputStream.write(buffer, 0, count);
 			}
 			fileInputStream.close();
-			
-			socket.close();
 		}
-	}
 }
